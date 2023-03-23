448195bcc8a89171376c6b18f46e355c93ecd5ac
Lluvia <friasllu@gmail.com>
2023-03-23 00:50:22 -0400

    commit tree 08caee20d89328d67098b152feafae8abe30d15a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+   badges=[]
+    attendees.collect { |name| badges <<  "Hello, my name is #{name}." }
+    badges
+end
+
+def assign_rooms(speakers)
+    room_assignments = []
+    room_num = 0
+
+    speakers.each_with_index { |speaker| room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room_num +=1}!" } 
+    
+    room_assignments  
+end
+
+def printer(attendees)
+  batch_badge_results = batch_badge_creator(attendees)
+  batch_badge_results.each { |line| puts line }
+
+  assign_rooms_result = assign_rooms(attendees)
+  assign_rooms_result.each {|line| puts line }
+end
